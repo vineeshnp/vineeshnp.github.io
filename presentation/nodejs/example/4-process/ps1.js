45bba152767bb597ce3cf0671e59b7efc4aa9255
vineeshnp <vineeshvalsalan@gmail.com>
2018-03-25 14:59:19 +0530

    Moving all presentations to seperate repository, hence removing from here.

--- presentation/nodejs/example/4-process/ps1.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-exec('cat *.js bad_file | wc -l', (error, stdout, stderr) => {
-	  if (error) {
-		      console.error(`exec error: ${error}`);
-		          return;
-			    }
-	    console.log(`stdout: ${stdout}`);
-	      console.log(`stderr: ${stderr}`);
-});
